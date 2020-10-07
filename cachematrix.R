497ea75a9b09343f04c1221277732903705d340f
vijaypothuganti <69586809+vijaypothuganti@users.noreply.github.com>
2020-10-07 17:19:02 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,6 +4,18 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inverse <- Null
+  set <- function(y){
+    x <<- y ##assign the input variable y into variable x
+    inverse <<- NULL ##define a function 'inverse' and initialize it to Null
+  }
+  get <- function() {x} ##return the matrix x
+  setInverse <- function(inverse) {inverse <<- inverse} ##set the makeCaheMatrix matrix equal to the inverse of matrix x
+  getInverse <- function() {inv} ##return the value of the inverse matrix x
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)n(x = matrix()) { ##this creates the special kind of matrix,it is acutailly a list
+
+}
+        
 
 }
 
@@ -11,5 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+          ##creates a function name cachesolve to find the inverse of the above special matrix
+        Inverse <- x$getInverse() 
+  if(!is.null(inverse)){  ##first check whether the inverse of the function has been calculated 
+    message("Getting  Chached Data")  
+    return(inverse).  ##if the inverse has been calculated, then it directly return the matrix above
+  }
+  matrix <- x$get() ##if the inverse has not been calculated, then it goes back to above function 'setInverse' to redo it again
+  inv <- solve(matrix, ...)
+  x$setInverse(inverse) 
+  inverse## Return a matrix that is the inverse of 'x'
 }
